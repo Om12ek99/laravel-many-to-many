0504c490b448ce7d801078235cec5670f93af3ff
Om12ek99 <omar.elkamili@gmail.com>
2024-06-26 15:39:50 +0200

    add: added seed of color in technology table

--- database/seeders/TechnologyTableSeeder.php
+++ database/seeders/TechnologyTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 
 class TechnologyTableSeeder extends Seeder
@@ -13,13 +14,15 @@ class TechnologyTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $tech_array = ['HTML/CSS', 'JS', 'PHP', 'VueJS', 'Laravel', 'Angular'];
 
         foreach ($tech_array as $tech) {
             $newTech = new Technology();
             $newTech->project_tech = $tech;
+            // restituisce una stringa CSS friendly
+            $newTech->description = $faker->safeColorName();
             $newTech->slug = Str::slug($tech);
             $newTech->save();
         }
