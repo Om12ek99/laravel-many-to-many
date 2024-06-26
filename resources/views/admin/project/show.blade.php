7d257609dc3464ab22d48b0444d1de6ec6bc23e3
Om12ek99 <omar.elkamili@gmail.com>
2024-06-27 01:59:26 +0200

    fix: naming

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -4,7 +4,7 @@
     <h1>{{$newProject->title}}</h1>
     <p> {{$newProject->content}} </p>
     <p>Slug: {{$newProject->slug}}</p>
-    <p>Tecnologia usata: {{ $newProject->type?->project_type }}</p>
+    <p>Tipologia del progetto : {{ $newProject->type?->project_type }}</p>
     <p> @foreach ($newProject->technologies as $technology)
         <p class="badge" style="background:{{$technology->description}}">{{ $technology->project_tech }}</p>
         @endforeach</p>
